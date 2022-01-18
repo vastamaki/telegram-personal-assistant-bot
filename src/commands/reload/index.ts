ce226dc1e0768fa11ec4180ba5a0646702c23805
Viljami Vastamäki <viljami.vastamaki@tutanota.com>
2022-01-18 17:33:05 +0200

    Add command descriptions (#7)
    * Add command descriptions
    
    * Fix a typo

---
+++ src/commands/reload/index.ts
@@ -0,0 +1,33 @@
+import { CustomContext } from "@functions/handler/handler";
+
+export default async (ctx: CustomContext) => {
+  await ctx.telegram.setMyCommands([
+    {
+      command: "start",
+      description: "Starts bot and registers your user",
+    },
+    {
+      command: "set_location",
+      description:
+        "Allows user to store location and activate commands that are requiring location data",
+    },
+    {
+      command: "weather",
+      description:
+        "Returns weather based on location stored using /setLocation command",
+    },
+    {
+      command: "empty",
+      description: "Clears waiting state",
+    },
+    {
+      command: "cmc",
+      description:
+        "Usage: /cmc add {symbol}, /cmc remove {symbol}, /cmc, returns cryptocurrency prices based on stored data to the user",
+    },
+    {
+      command: "reload",
+      description: "Reloads this list of commands",
+    },
+  ]);
+};
